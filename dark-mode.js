d2411f780cfad2c97c2ee9c2f7ee31aef72e7e99
KK-Designs <71038229+KK-Designs@users.noreply.github.com>
2021-11-01 19:40:28 -0700

    Formatted `javascript`

--- dark-mode.js
+++ dark-mode.js
@@ -1,7 +1,7 @@
 function changeToDarkMode(settings) {
-	const element = document.body;
-	element.classList.toggle("dark-mode");
-	if (settings && settings.smooth_transition) {
-		element.classList.toggle("smooth-transition");
-	}
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+    if (settings && settings.smooth_transition) {
+        element.classList.toggle("smooth-transition");
+    }
 }
